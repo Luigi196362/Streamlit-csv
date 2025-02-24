10adedb46ac8f68ae2c81e7fc2f9b5dd2b01931f
Luis Enrique Romero Perez <wichobros@gmail.com>
2025-02-23 21:09:53 -0600

    Credencial

--- data.py
+++ data.py
@@ -5,3 +5,12 @@ names_data = pd.read_csv(names_link)
 # Create the title for the web app
 st.title("Streamlit and pandas")
 st.dataframe(names_data)
+
+
+sidebar = st.sidebar
+
+sidebar.title("Luis Enrique Romero Pérez")
+
+sidebar.write("Matricula: zs21004524.")
+
+sidebar.image("https://firebasestorage.googleapis.com/v0/b/paradigmas-luigi196362.appspot.com/o/javascript%2Fimages%2Fcredencial.jpg?alt=media&token=ffba3513-0c65-4540-8bdf-140a9ba8f468")
